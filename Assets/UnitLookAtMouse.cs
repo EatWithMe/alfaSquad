706d45eafa52be7cc0c4503eddb855d44009096b
EatWithMe <alfa2alfa2@mail.ru>
2016-01-28 03:32:36 +0300

    add working Unit look at mouse

---
+++ Assets/UnitLookAtMouse.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitLookAtMouse : MonoBehaviour {
+
+    public float rotationSpeedMax = 200;//angels per sec
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateUnitToMouse();
+    }
+
+    void RotateUnitToMouse()
+    {
+        Vector3 RotateToTarget;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            RotateToTarget = hit.point;
+            //Debug.Log(RotateToTarget);
+        }
+        else
+        {
+            return;
+        }
+
+
+
+        Vector3 diff = RotateToTarget - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0f, 0f, rot_z - 90), rotationSpeedMax * Time.deltaTime);
+
+    }
+}
+
+
